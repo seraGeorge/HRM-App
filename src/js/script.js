f864f59e9cf790bc4f24b6024e96d5c05358d273
Sera Mariam George <leelasera19@gmail.com>
2023-10-04 02:20:20 +0530

    [add]: loader partially done

--- src/js/script.js
+++ src/js/script.js
@@ -48,3 +48,16 @@ onValue(readUserData, (snapshot) => {
     }
     tableData.innerHTML += temp
 });
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+            "body").style.visibility = "hidden";
+        document.querySelector(
+            "#loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+            "#loader").style.display = "none";
+        document.querySelector(
+            "body").style.visibility = "visible";
+    }
+};
